a72e1ed3d6342736144a4a63784826430aa27ab3
znuzi <micke.s.a@outlook.com>
2025-11-28 23:17:23 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,1 @@
-SYMBOLS = {
-    'BTC': {'pyth_id': 'e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43'},
-    'ETH': {'pyth_id': 'ff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace'},
-    'SOL': {'pyth_id': 'ef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d'},
-}
 
-Z_ENTRY = 2.0
-Z_EXIT = 0.2
-POSITION_SIZE_USD = 100
-POLL_INTERVAL = 3
-# seconds
